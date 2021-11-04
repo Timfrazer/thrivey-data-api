7fa5a19f0ecb66904885a08727070d63225ed1f0
Sean Glynn <sean.glynn@thriveglobal.com>
2021-11-04 15:35:13 +0000

    Simplifying DB connector

--- app/db/connection.py
+++ app/db/connection.py
@@ -5,30 +5,30 @@ from sqlalchemy.orm import sessionmaker
 import databases
 
 
-DEFAULT_SQLITE_DB='sqlite:///data/thrivey.db'
-SQLALCHEMY_DATABASE_URL = config("DATABASE_URL", DEFAULT_SQLITE_DB)
-print(SQLALCHEMY_DATABASE_URL)
-
-import os
-print(os.getcwd())
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
-def get_sesh():
-    sesh = SessionLocal()
-    try:
-        yield sesh
-    finally:
-        sesh.close()
-
-def get_db():
-    database = databases.Database(DEFAULT_SQLITE_DB)
+def init_engine(db_url):
+    engine = create_engine(
+        db_url, 
+        connect_args={"check_same_thread": False}
+    )
     metadata = MetaData()
-
     metadata.create_all(engine)
 
+    return metadata
+
+
+def get_db(db_url):
+    database = databases.Database(db_url)
+
     return database
+
+
+
+
+# def get_sesh():
+#     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#     Base = declarative_base()
+#     sesh = SessionLocal()
+#     try:
+#         yield sesh
+#     finally:
+#         sesh.close()
