59d337f3c3546c01710326a3766dad24cec9d59d
Sean Glynn <sean.glynn@thriveglobal.com>
2021-11-04 15:34:19 +0000

    Updating API models

--- app/db/model.py
+++ app/db/model.py
@@ -1,42 +1,45 @@
 import uuid
 
 from sqlalchemy import Column, String, Integer
-from sqlalchemy import MetaData, Table
+from sqlalchemy import Table
+from sqlalchemy.ext.declarative import declarative_base
 
-from db.connection import Base, engine
+# from db.connection import engine
+# Base = declarative_base()
+# metadata = MetaData()
 
-metadata = MetaData()
-
-user_behaviour_tbl = Table(
-    "user_behaviours",
-    metadata,
-    Column("index", Integer, primary_key=True),
-    Column("user_id", Integer),
-    Column("first_name", String),
-    Column("last_name", String),
-    Column("user_created", String),
-    Column("thrivey_score", Integer),
-    Column("user_action", String),
-    Column("date_created", String),
-    Column("ipv4", String),
-)
-
-
-
-class UserBehaviour(Base):
-    __tablename__ = "user_behaviours"
-
-    index = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer)
-    first_name = Column(String)
-    last_name = Column(String)
-    user_created = Column(String)
-    thrivey_score = Column(Integer)
-    user_action = Column(String)
-    date_created = Column(String)
-    ipv4 = Column(String)
+def user_behaviour_table_model(metadata):
+    user_behaviour_tbl = Table(
+        "user_behaviours",
+        metadata,
+        Column("index", Integer, primary_key=True),
+        Column("user_id", Integer),
+        Column("first_name", String),
+        Column("last_name", String),
+        Column("user_created", String),
+        Column("thrivey_score", Integer),
+        Column("session_id", Integer),
+        Column("user_action", String),
+        Column("date_created", String),
+        Column("ipv4", String),
+    )
+    return user_behaviour_tbl
 
 
+# class UserBehaviourModel(Base):
+#     __tablename__ = "user_behaviours"
+
+#     index = Column(Integer, primary_key=True, index=True)
+#     user_id = Column(Integer)
+#     first_name = Column(String)
+#     last_name = Column(String)
+#     user_created = Column(String)
+#     thrivey_score = Column(Integer)
+#     user_action = Column(String)
+#     date_created = Column(String)
+#     ipv4 = Column(String)
 
 
-Base.metadata.create_all(engine)
+
+
+# # Base.metadata.create_all(engine)
--- app/models/UserBehaviour.py
+++ app/models/UserBehaviour.py
@@ -1,20 +1,22 @@
 from pydantic import BaseModel
-
+from typing import  Optional
 
 class User(BaseModel):
+    user_id: int
     first_name: str
     last_name: str
     user_created: str
     thrivey_score: int
     
 class Behaviour(BaseModel):
+    user_id: int
+    session_id: int
     user_action: str
     date_created: str
     ipv4: str
 
 class UserBehaviour(BaseModel):
     id: int
-    user_id: str
     user: User
     behaviour: Behaviour
 
