fd2fe3da969573715255b87b95c9487caaaf73b3
Sean Glynn <sean.glynn@thriveglobal.com>
2021-11-05 14:11:51 +0000

    Updating tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,54 @@
 import pytest
 
-def test_ping(test_app):
+mocked_record={
+    "id": 0,
+    "user": {
+        "user_id": 1,
+        "first_name": "John",
+        "last_name": "Fakington",
+        "user_created": "Sep 14, 2021 4:32 AM",
+        "thrivey_score": 99
+    },
+    "behaviour": {
+        "user_id": 1,
+        "session_id": 1,
+        "user_action": "A1",
+        "date_created": "Oct 17, 2021 5:23 AM",
+        "ipv4": "10.0.0.1"
+    }
+}
+
+
+def test_root(test_app):
     response = test_app.get("/")
     assert response.status_code == 200
     assert response.json() == {"Thrivey": "OK"}
 
 
 
+def test_validate_valid_doc_returns_success(test_app):
+    response = test_app.post(
+        "/user_behaviour/validate",
+        json=mocked_record,
+    )
+    assert response.status_code == 200
+    assert response.json() == {'isValid': True}
+
+
+
+def test_validate_invalid_doc_returns_422(test_app):
+
+    # Update record with invalid type
+    mocked_record['id'] = "ThisIsAnInvalidType"
+
+    response = test_app.post(
+        "/user_behaviour/validate",
+        headers={"X-Token": "coneofsilence"},
+        json=mocked_record,
+    )
+
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == "value is not a valid integer"
 
 
 
