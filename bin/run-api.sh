6a754dceb892cd934eea14d2758d48115d4cdfaf
Sean Glynn <sean.glynn@thriveglobal.com>
2021-11-08 12:36:00 +0000

    Integrating logger

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -5,5 +5,6 @@ if [ -z "$API_PORT"  ] && [ -z "$API_WORKER_COUNT" ];then
     API_WORKER_COUNT=2
 fi
 
+
 poetry run uvicorn --reload --log-level debug --host=0.0.0.0 --port ${API_PORT} \
-    --workers ${API_WORKER_COUNT} app.main:api
+    --workers ${API_WORKER_COUNT} --log-config logger-conf.yaml app.main:api
