a392ca7c83c2411d4c85bd17322e4d80a442adf8
Sean Glynn <sean.glynn@thriveglobal.com>
2021-11-09 11:39:50 +0000

    Adding JSON data generator

---
+++ bin/generate_userbehaviour_rows.py
@@ -0,0 +1,24 @@
+from app.utils.data_generator import add_n_more_dummy_rows
+import json
+import argparse
+
+# Define the parser
+parser = argparse.ArgumentParser(description='Generate JSON UserBehaviour Data')
+parser.add_argument('--rows', action="store", dest='rows', type=int, required=True)
+
+args = parser.parse_args()
+num_rows = args.rows
+
+# Read sample json
+json_file='data/user_behaviour.json'
+with open(json_file) as user_json_file:
+    json_data = json.load(user_json_file)
+
+# Return new JSON Collection
+new_json = add_n_more_dummy_rows(json_data, num_rows)
+
+# Write out data
+json_file_out='data/user_behaviour_{0}.json'.format(num_rows)
+
+with open(json_file_out, 'w') as outfile:
+    json.dump(new_json, outfile)
