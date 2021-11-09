7fe5b41214522c6ae35074d5dacd2197c7c7a8dc
Sean Glynn <sean.glynn@thriveglobal.com>
2021-11-09 16:29:26 +0000

    Added dodgy endpoint for testing HTTPException handling

--- app/routes/user_behaviour_json.py
+++ app/routes/user_behaviour_json.py
@@ -4,9 +4,11 @@ import json
 
 from fastapi import APIRouter, HTTPException, Path
 from fastapi_pagination import Page, add_pagination, paginate
+import random
 
 from app.models.UserBehaviour import UserBehaviour
 from app.main import api
+from app.utils import http_error_generator
 
 # Init router
 user_behaviour_json_router = APIRouter()
@@ -22,12 +24,25 @@ with open(json_file) as user_json_file:
 with open(json_file10k) as user_json_file10k:
     json_data10k = json.load(user_json_file10k)
 
+
+
+
 # Load 150 records straight up
 @api.get("/user_behaviour/json", response_model=list[UserBehaviour])
 async def read_user_behaviour_json():
     result = (json_data)
     logging.info("result: %s", result)
     return result
+
+# HTTP endpoint that will fail 4/5 times
+# Either pass the param[1-5] else randomly generate a request outcome each GET
+@api.get("/user_behaviour/dodgy_endpoint")
+async def read_user_behaviour_dodgy_endpoint(http_req_outcome: int = random.randint(1, 5)):
+    
+    http_error_generator.generate_dummy_resp(http_req_outcome)
+
+    # If option 1 occurs -> Successful response returned. Else HTTPException
+    return json_data
 
 # Load 10,000 recs with pagination
 @api.get("/user_behaviour_10k/json", response_model=Page[UserBehaviour])
---
+++ app/utils/http_error_generator.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException
+
+
+# Return 1 of 5 dummy HTTP Responses
+def success():
+    print("Successful Response!")
+
+def bad_request():
+    raise HTTPException(status_code=400, detail="Validation Failed!")
+
+def not_found():
+    raise HTTPException(status_code=404, detail="Item not found!")
+
+def unauthourized():
+    raise HTTPException(status_code=401, detail="Unauthorized!")
+
+def server_error():
+    raise HTTPException(status_code=500, detail="Server Error!")
+
+def generate_dummy_resp(choice):
+  options = {1 : success,
+            2 : bad_request,
+            3 : not_found,
+            4 : unauthourized,
+            5 : server_error,
+  }
+  options[choice]()
+
