4168eadad8b268916ff8addd3d77d69ea22bd341
Sean Glynn <sean.glynn@thriveglobal.com>
2021-11-09 15:17:40 +0000

    Adding dummy data generator func

--- app/utils/data_generator.py
+++ app/utils/data_generator.py
@@ -11,7 +11,8 @@ fake.add_provider(date_time)
 user_behaviour_actions = ['A1', 'A3', 'B6', 'B12', 'C3', 'C8', 'D3', 'D8', 'E3', 'E8', 'F3', 'F8', 'G3', 'G8', 'H3', 'H8', 'I3', 'I8', 'J3', 'J8', 'K3', 'K8']
 
 # datetime format
-datetime_format='%b %d, %Y %-I:%M %p'
+default_datetime_format='%b %d, %Y %-I:%M %p'
+iso_8601_format="%Y%m%dT%H%M%S.%fZ"
 
 # Generate IPV4 column for Pandas DF
 def add_fake_ipv4_col(pandas_df):
@@ -19,13 +20,12 @@ def add_fake_ipv4_col(pandas_df):
         pandas_df.loc[index,'ipv4'] = fake.ipv4_private()
     return pandas_df
 
-# Generate n more json objects from dict and return larger dict object
-def add_n_more_dummy_rows(list_of_dicts: list, increase_rows: int):
+# Generate n more json objects from dict and return larger dict object. CAUTION $$$$$$$EXPENSIVE$$$$$$$$!
+def add_n_more_dummy_rows(list_of_dicts: list, increase_rows: int, dt_format=default_datetime_format):
     maxIdIter = max(list_of_dicts, key=lambda x:x['id'])['id']
     maxUserIdIter = max(list_of_dicts, key=lambda x:x['user']['id'])['id']
     print(f"maxIdIter: {maxIdIter}")
     print(f"maxUserIdIter: {maxUserIdIter}")
-
 
     for i in range(increase_rows):
         maxIdIter += 1
@@ -37,16 +37,14 @@ def add_n_more_dummy_rows(list_of_dicts: list, increase_rows: int):
                 "id": maxUserIdIter,
                 "first_name": fake.first_name(),
                 "last_name": fake.last_name(),
-                "user_created": fake.date_time_ad(start_datetime='-5m', end_datetime='-4m').strftime(format=datetime_format),
-                # "user_created": fake.date_time_ad(start_datetime=datetime('2021-09-01 00:00:00'), end_datetime=datetime('2021-09-30 23:59:00')),
+                "user_created": fake.date_time_ad(start_datetime='-5m', end_datetime='-4m').strftime(format=dt_format),
                 "thrivey_score": fake.pyint(min_value=1, max_value=100),
             },
             "behaviour": {
                 "user_id": maxUserIdIter,
                 "session_id": fake.pyint(min_value=1, max_value=3),
                 "user_action": fake.word( user_behaviour_actions),
-                "date_created": fake.date_time_ad(start_datetime='-4m', end_datetime='-3m').strftime(format=datetime_format),
-                # "date_created": fake.date_time_ad(start_datetime=datetime('2021-10-01 00:00:00'), end_datetime=datetime('2021-10-30 23:59:00')),
+                "date_created": fake.date_time_ad(start_datetime='-4m', end_datetime='-3m').strftime(format=dt_format),
                 "ipv4": fake.ipv4_private(),
             }
         }
